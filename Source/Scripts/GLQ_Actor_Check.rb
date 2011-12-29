32cdb2f455456d5cf84eec0a624078fd78fbe25c
gegerlan <gegerlan2@hotmail.com>
2011-12-29 16:41:07 +0000

    Added dice checks to Game_Interpreter.
    Extended Game_Interpreter to allow check calls directly in the interpreter. See Game_Actor.check for a better description of the parameters.
    checkParty(difficulty, attribute[, modifier[, modifier[, ...]]]) - See if any member of the party passes the check.
    checkPlayer(difficulty, attribute[, modifier[, modifier[, ...]]]) - See if the current leader of the party passes the check.
    check(difficulty, attribute[, modifier[, modifier[, ...]]]) - alias for checkParty
    checkActor(actorIdentifier, difficulty, attribute[, modifier[, modifier[, ...]]]) - check actor (as defined in database) by using object (Game_Actor), database position (integer) or name (string)
    checkMember(memberIdentifier, difficulty, attribute[, modifier[, modifier[, ...]]]) - check current party member by using object (Game_Actor), party position (integer) or name (string)
    
    Usage:
    
      >Conditional Branch: Script: check(50, sexAppeal)
        >Comment: successful dice roll, someone in the party is considered "sexy enough'.
        >Text: Hawt damn!
      : Else
        >Comment: unsuccessful dice roll
        >Text: Hello...
      : Branch End

--- Source/Scripts/GLQ_Actor_Check.rb
+++ Source/Scripts/GLQ_Actor_Check.rb
@@ -30,3 +30,45 @@ class Game_Actor
     return outcome
   end
 end
+
+class Game_Interpreter
+  def checkParty(difficulty, attributeName, *modifiers)
+    success = false
+    $game_party.members.each do |member|
+      success = member.check(difficulty, attributeName, *modifiers)
+      break if success
+    end
+    return success
+  end
+  def checkPlayer(difficulty, attributeName, *modifiers)
+    return $game_party.members[0].check(difficulty, attributeName, *modifiers)
+  end
+  alias check checkParty
+  def checkActor(actorIdentifier, difficulty, attributeName, *modifiers)
+    actors = $game_actors.to_a.slice(1..-1)
+    actor = getActor(actorIdentifier, actors)
+    if actor != nil
+      return actor.check(difficulty, attributeName, *modifiers)
+    end
+    return false
+  end
+  def checkMember(actorIdentifier, difficulty, attributeName, *modifiers)
+    actor = getActor(actorIdentifier, $game_paty.members)
+    if actor != nil
+      return actor.check(difficulty, attributeName, *modifiers)
+    end
+    return false
+  end
+  private
+  def getActor(actorIdentifier, context)
+    if actorIdentifier.is_a?(Game_Actor)
+      return actorIdentifier
+    elsif actorIdentifier.is_a?(Integer)
+      return context[actorIdentifier]
+    elsif actorIdentifier.is_a?(String)
+      return context.find{|actor| actor.name == actorIdentifier}
+    else
+      return nil
+    end
+  end
+end
