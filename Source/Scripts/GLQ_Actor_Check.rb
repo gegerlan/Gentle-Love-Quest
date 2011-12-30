0e22413727a17fa320d02a70f8e0a1e98a53c6b4
gegerlan <gegerlan2@hotmail.com>
2011-12-30 23:44:15 +0000

    Documented GLQ_Actor_Check.

--- Source/Scripts/GLQ_Actor_Check.rb
+++ Source/Scripts/GLQ_Actor_Check.rb
@@ -1,4 +1,22 @@
+=begin
+# Added dice rolls for doing checks against attributes
+=end
 class Game_Actor
+  # Do a dice roll and see if the actor passes the check
+  # check(difficulty, attributeName[, modifier[, modifier[, ..])
+  #   difficulty: How likely the check is to pass (recommended 0-100 value)
+  #               0 implies always, while 100 makes it very unlikely
+  #   attributeName: The name of the actor's attributes to check
+  #                  e.g. fertility. The value is converted to int for the
+  #                  check
+  #   modifier: positive or negative values that impacts the likelyhood that
+  #             the check is passed by the actor. This could be context specific
+  #             things like dialog choices.
+  #
+  # Example
+  #   if actor.check(80, fertility)
+  #     p "Congratulation #{actor.name} is pregnant!"
+  #   end
   def check(difficulty, attributeName, *modifiers)
     attributeValue = send attributeName
     attributeValue = attributeValue.to_i
