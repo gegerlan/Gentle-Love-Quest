16608e005f72470f2da64bc285e4704871942122
gegerlan <gegerlan2@hotmail.com>
2011-12-31 17:38:37 +0000

    Added script to track face file and face index use.
    face_name (name of the face file) is now based on the character name and clothes. (e.g. Eva - Normal)
    face_index what section (of the 8 possible) of the face file should be used. This is based on the mood of the player. Currently hard coded for Eva use.

--- Source/Scripts/GLQ_Actor_Portrait.rb
+++ Source/Scripts/GLQ_Actor_Portrait.rb
@@ -2,4 +2,23 @@ class Game_Actor < Game_Battler
   def portrait_name
     "#{name} - #{clothing} - #{mood}" # Eva - Normal - Smiling
   end
+  # Returns the name of the file used as the face for the actor.
+  def face_name
+    "#{name} - #{clothing}"
+  end
+  # Returns what part of the face image that should be used for the actor.
+  def face_index
+    case mood
+    when /Neutral/i
+      0
+    when /Empty eyes/i
+      1
+    #when ???
+    # 2
+    when /Ahegao/i
+      3
+    else
+      0
+    end
+  end
 end
