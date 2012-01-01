edb1d5d553a734bb5f100aa2c890967a4ebac4e5
gegerlan <gegerlan2@hotmail.com>
2012-01-01 20:30:10 +0000

    Update attribute history.
    Refactored large parts of raise and lower.
    Added :update_attribute(attribute, delta, context) that does the actual logging of the change and makes the change to the attribute.
    Added the ability to pass a block to :get_context_occurance [sic] to further limited what's selected for counting.

--- Source/Scripts/GLQGame_Actor.rb
+++ Source/Scripts/GLQGame_Actor.rb
@@ -66,18 +66,45 @@ class Game_Actor < Game_Battler
     return sizes[index]
   end
   
-  def get_context_occurance(attribute, context)
+  # Provides the number of matched context
+  #   attribute: name of the attribute
+  #   context: the context to match for the attribute
+  #   block: further selection criteria as a block
+  #
+  # Example
+  #    get_context_occurance("sexAppeal", [1,2,3]) # Get the number of changes to sexAppeal from map 1's event 2's page 3
+  #    get_context_occurance("fertility", [3,2,1], { |item| item[:value] > 0 }) # Get the number of changes to fertility from map 3's event 2's page 1 that were positive.
+  def get_context_occurance(attribute, context, &block)
     history = @attribute_history[attribute] ||= []
     context_instances = history.select { |instance| instance[:context] == context }
-    context_instances.length
+    if context_instances != nil
+      context_instances = context_instances.select {|item| block.call(item)} if block != nil
+      context_instances.length
+    else
+      0
+    end
   end
-  
+  # Update the value of a historic attribute change
+  #   attribute = attribute name
+  #   time = the game frame when it happened
+  #   context = the context where the change happened
+  #   value = the value to change to
   def update_history(attribute, time, context, value)
     history = @attribute_history[attribute] ||= []
     i = history.find { |item| item[:time] == time && item[:context] == context }
     i[:value] = value
   end
-  
+
+  # Remove a historic attribute change
+  #   attribute = name of the attribute
+  #   context = the change at a given context to remove
+  #   time = the change at the given time to remove
+  # Context and/or time needs to be provided. But both is not a must.
+  #
+  # Example
+  #   remove_history("sexAppeal", [1,2,3]) # remove historic change to sexAppeal for Map 1's Event 2's Page 3
+  #   remove_history("fertility", nil, 130) # remove historic change to fertility that happened during frame 130 (~2 secounds into the game)
+  #   remove_history("pervert", [3,2,1], 1200) # remove historic change to pervert that happened for Map 3's Event 2's Page 1, during frame 1200 (~2 minutes into the game)
   def remove_history(attribute, context = nil, time = nil)
     if context != nil || time != nil
       history = @attribute_history[attribute] ||= []
@@ -93,7 +120,10 @@ class Game_Actor < Game_Battler
       end
     end
   end
-  
+  # Log a change to an attribute
+  #   attribute = name of the attribute
+  #   context = where the change happened
+  #   value = the change (delta)
   def add_history(attribute, context, value)
     time = Graphics.frame_count
     item = {:time => time, :context => context, :value => value}
@@ -108,32 +138,26 @@ class Game_Actor < Game_Battler
   #   max: the highest value the raise can increase towards (i.e. attribute can't be raised beyond this value)
   #   context: the context uniquely identifying the change
   def raise(attribute, delta, max, context = nil)
-    if context != nil
-      # If we have a context, we want to see how often this change has been
-      # called and make appropriate changes to our delta value.
-      delta = delta / (get_context_occurance(attribute, context) + 1)
-      delta = delta.floor
-    end
-    if delta == 0 
-      # If there is no change, just return the attribute value
-      self.send attribute
+    current_value = self.send attribute
+    if max < current_value 
+      # Skip if the current value is less than the min value provided
+      current_value
     else
-      current_value = self.send attribute
-      if max < current_value
-        # Skip if the current value is greater than the max value provided
-        current_value
+      if context != nil
+        # If we have a context, we want to see how often this change has been
+        # called and make appropriate changes to our delta value, by halfing
+        # the change with the number of times the update has been called.
+        delta = delta / (get_context_occurance(attribute, context) {|item| delta > 0 ? item[:value] > 0 : item[:value] < 0 } + 1)
+        delta = delta.floor
+      end
+      if current_value + delta.abs > max
+        delta = max - current_value
+      end
+      if delta == 0
+        current_value # ignore if there's no change
       else
-        target_value  = current_value + delta.abs
-        if target_value > max
-          # If the projected value is greater than max, set the new value to max
-          target_value = max
-        end
-        # Add the change to the attribute history
-        add_history(attribute, context, target_value - current_value)
-        # Makes a call setting the attribute
-        # e.g. sexAppeal=10
-        self.send("#{attribute}=", target_value)
-      end      
+        update_attribute(attribute, delta.abs, context)
+      end
     end
   end
   # Lower one of the attributes for the actor.
@@ -142,32 +166,38 @@ class Game_Actor < Game_Battler
   #   min: the smallest value the lower can decrease towards (i.e. attribute can't be lowered beyond this value)
   #   context: the context uniquely identifying the change
   def lower(attribute, delta, min, context = nil)
-    if context != nil
-      # If we have a context, we want to see how often this change has been
-      # called and make appropriate changes to our delta value.
-      delta = delta / (get_context_occurance(attribute, context) + 1)
-      delta = delta.floor
+    current_value = self.send attribute
+    if min > current_value 
+      # Skip if the current value is less than the min value provided
+      current_value
+    else
+      if context != nil
+        # If we have a context, we want to see how often this change has been
+        # called and make appropriate changes to our delta value, by halfing
+        # the change with the number of times the update has been called.
+        delta = delta / (get_context_occurance(attribute, context) {|item| delta > 0 ? item[:value] > 0 : item[:value] < 0 } + 1)
+        delta = delta.floor
+      end
+      if current_value - delta.abs < min
+        delta = min - current_value
+      end
+      if delta == 0
+        current_value # ignore if there's no change
+      else
+        update_attribute(attribute, -delta.abs, context)
+      end
     end
+  end
+  
+  def update_attribute(attribute, delta, context)
     if delta == 0 
       # If there is no change, just return the attribute value
       self.send attribute
     else
-      current_value = self.send attribute
-      if min > current_value 
-        # Skip if the current value is less than the min value provided
-        current_value
-      else
-        target_value  = current_value - delta.abs
-        if target_value < min
-          # If the projected value is less than min, set the new value to min
-          target_value = min
-        end
-        # Add the change to the attribute history
-        add_history(attribute, context, target_value - current_value)
-        # Makes a call setting the attribute
-        # e.g. sexAppeal=10
-        self.send("#{attribute}=", target_value)
-      end
+      add_history(attribute, context, delta)
+      # Makes a call setting the attribute
+      # e.g. sexAppeal=10
+      self.send("#{attribute}=", self.send(attribute) + delta)
     end
   end
   
