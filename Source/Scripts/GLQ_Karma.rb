c341ef5903eefcbf84d8f8e14db64ad8ac07550b
gegerlan <gegerlan2@hotmail.com>
2012-01-02 20:03:17 +0000

    Added karma for $game_player object
    Extended Game_Player with Karma. This makes it available through the $game_player object, and is stored in the save games by default. By not storing it in $variables_data it won't be affected if all the variables needs to be reset, e.g. for a Game+ mode.
    
    Example:
        $game_player.karma = 1
          => 1
        $game_player.karma += 3
          => 4
        $game_player.karma -= 4
          => 0

---
+++ Source/Scripts/GLQ_Karma.rb
@@ -0,0 +1,8 @@
+class Game_Player < Game_Character
+  attr_accessor   :karma       # type of vehicle currenting being ridden
+  alias glq_karma_initialize initialize
+  def initialize
+    glq_karma_initialize
+    @karma = 0
+  end
+end
