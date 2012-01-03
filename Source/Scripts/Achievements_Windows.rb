6f258bad0dc098ccce25f1588434542025d7119c
gegerlan <gegerlan2@hotmail.com>
2012-01-03 00:03:49 +0000

    Rewrote Achievements storage.
    Achievements is now a singleton containing all achievements and their state.
    
    Achievements
      SAVE_FILE = "Data/Achievements.rvdata"
        Where the completed achievements are stored
      self.instance
        returns instantiated Achievements
      :[index]
        returns achievement at index
      :get(index)
        returns achievement at index
      :size
        returns number of achievements
      :load_data
        loads saved achievement definition and completions into memory
      :save
        stores the completed achievements
      :clear
        removes all achievements
    
    Since the class is a singleton, you'll need to call :instance to make sure that the same Achievements are being worked on.
    
    Example
        @achievements_i1 = Achievements.instance
        @achievements_i2 = Achievements.instance # points to @achievements_i1
        @achievements_n = Achievements.new
        @achievements_i1.get(1).completed?
          => false
        @achievements_i2.get(1).completed?
          => false
        @achievements_n.get(1).completed?
          => false
        @achievements_i1.get(1).completed = true
          => true
        @achievements_i2.get(1).completed?
          => true # true since pointing to the value of @achievements_i1
        @achievements_n.get(1).completed?
          => false # separate object
    
    This can be a blessing or a curse, depending on who is coding. Just be aware of it.
    
    Achievements in turn are stored as Achievement objects
      :title
        returns the title of the achievement
      :description
        returns the description of the achievement
      :completed
        returns or sets completed status (TRUE/FALSE)
      :completed?
        returns true if achievement is completed
    
    Scene and Window classes that uses Achievements has been updated to use the new definitions.

--- Source/Scripts/Achievements_Windows.rb
+++ Source/Scripts/Achievements_Windows.rb
@@ -1,94 +1,156 @@
-class Game_System
-  alias emt_initialize initialize
-  
+# Contains all achievement in the game
+class Achievements
+  # Default achievements
+  ACHIEVEMENTS = {
+    0  => ["Like A Virgin", false, "Fucked for the very first time"],
+    1  => ["Nymphomania", false, "Fucked 1,000 times"],
+    2  => ["Sex bomb", false, "Fucked 1,000,000 times"],
+
+    3  => ["Exploration", false, "Masturbated"],
+    4  => ["Self Esteem", false, "Masturbated 1,000 times"],
+    5  => ["Narcissism", false, "Masturbated 1,000,000 times"],
+    
+    6  => ["My First Candle", false, "Sucked a cock"],
+    7  => ["Like The Wind", false, "Sucked 1,000 cocks"],
+    8  => ["Cock Smoker!", false, "Sucked 1,000,000 cocks"],
+    
+    9  => ["No Pain, No Gain", false, "Fucked in the ass"],
+    10 => ["Vaseline Optional", false, "Fucked in the ass 1,000 times"],
+    11 => ["Where's The Backdoor?", false, "Fucked in the ass 1,000,000 times"],      
+    
+    12 => ["Experimentation", false, "Fucked by a woman"],
+    13 => ["Indiscrimination", false, "Fucked by a woman 1,000 times"],
+    14 => ["Thanks For All The Fish", false, "Fucked by a woman 1,000,000 times"],
+    
+    15 => ["Inibitions, me?", false, "Exposed indecently to someone"],
+    16 => ["The Naked Maja", false, "Exposed indecently to 1,000 people"],
+    17 => ["Lady Godiva", false, "Exposed indecently to 1,000,000 people"],
+
+    18 => ["A Rock And A Hard Place", false, "Worked as whore"],
+    19 => ["Like A Pro", false, "Worked as whore 1,000 times"],
+    20 => ["A Ticket to Paradise", false, "Worked as whore 1,000,000 times"],
+
+    21 => ["My New Mascot", false, "Fucked by an animal"],
+    22 => ["Animal Love", false, "Fucked by 1,000 animals"],
+    23 => ["Who's The Pet Now?", false, "Fucked by 1,000,000 animals"],
+    
+    24 => ["Poke-lover", false, "Fucked by a Pokemon"],
+    25 => ["Gotta Catch Them All", false, "Fucked by 1,000 pokemons"],
+    26 => ["Pokemon Mistress", false, "Fucked by 1,000,000 pokemons"],
+
+    27 => ["Naughty Spiral", false, "Fucked under mind control"],
+    28 => ["Lose Your Senses", false, "Fucked under mind control 1,000 times"],
+    29 => ["Fuck Doll", false, "Fucked under mind control 1,000,000 times"],
+
+    30 => ["An Unwanted Guest", false, "Raped"],
+    31 => ["Broken Lock", false, "Raped 1,000 times"],
+    32 => ["Free Buffet", false, "Raped 1,000,000 times"],
+    
+    33 => ["Got Milk?", false, "Produced 1 liter of milk"],
+    34 => ["Udderly delicious", false, "Produced 1,000 liter of milk"],
+    35 => ["The Milky Way", false, "Produced 1,000,000 liter of milk"],
+
+    36 => ["Zidane", false, "Rescue Vivi! She needs help!"],
+    37 => ["For the Horde!", false, "Enslaved by The Horde"],
+    38 => ["The Beast's Pet", false, "Enslaved by an Orc Soldier"],      
+    39 => ["Lost Lost Number", false, "Became Lost Number's mascot"],
+    40 => ["White Knight", false, "Rescue Princess Elisabeth"],
+    41 => ["Green Kids", false, "Enslaved for Orc Breeding"]
+  }
+  # Where unlocked achievements are stored
+  SAVE_FILE = "Data/Achievements.rvdata"
   def initialize
-    emt_initialize
-    @Achievements = {
-      0  => ["Like A Virgin", false, "Fucked for the very first time"],
-      1  => ["Nymphomania", false, "Fucked 1,000 times"],
-      2  => ["Sex bomb", false, "Fucked 1,000,000 times"],
- 
-      3  => ["Exploration", false, "Masturbated"],
-      4  => ["Self Esteem", false, "Masturbated 1,000 times"],
-      5  => ["Narcissism", false, "Masturbated 1,000,000 times"],
-      
-      6  => ["My First Candle", false, "Sucked a cock"],
-      7  => ["Like The Wind", false, "Sucked 1,000 cocks"],
-      8  => ["Cock Smoker!", false, "Sucked 1,000,000 cocks"],
-      
-      9  => ["No Pain, No Gain", false, "Fucked in the ass"],
-      10 => ["Vaseline Optional", false, "Fucked in the ass 1,000 times"],
-      11 => ["Where's The Backdoor?", false, "Fucked in the ass 1,000,000 times"],      
-      
-      12 => ["Experimentation", false, "Fucked by a woman"],
-      13 => ["Indiscrimination", false, "Fucked by a woman 1,000 times"],
-      14 => ["Thanks For All The Fish", false, "Fucked by a woman 1,000,000 times"],
-      
-      15 => ["Inibitions, me?", false, "Exposed indecently to someone"],
-      16 => ["The Naked Maja", false, "Exposed indecently to 1,000 people"],
-      17 => ["Lady Godiva", false, "Exposed indecently to 1,000,000 people"],
-
-      18 => ["A Rock And A Hard Place", false, "Worked as whore"],
-      19 => ["Like A Pro", false, "Worked as whore 1,000 times"],
-      20 => ["A Ticket to Paradise", false, "Worked as whore 1,000,000 times"],
-
-      21 => ["My New Mascot", false, "Fucked by an animal"],
-      22 => ["Animal Love", false, "Fucked by 1,000 animals"],
-      23 => ["Who's The Pet Now?", false, "Fucked by 1,000,000 animals"],
-      
-      24 => ["Poke-lover", false, "Fucked by a Pokemon"],
-      25 => ["Gotta Catch Them All", false, "Fucked by 1,000 pokemons"],
-      26 => ["Pokemon Mistress", false, "Fucked by 1,000,000 pokemons"],
-
-      27 => ["Naughty Spiral", false, "Fucked under mind control"],
-      28 => ["Lose Your Senses", false, "Fucked under mind control 1,000 times"],
-      29 => ["Fuck Doll", false, "Fucked under mind control 1,000,000 times"],
-
-      30 => ["An Unwanted Guest", false, "Raped"],
-      31 => ["Broken Lock", false, "Raped 1,000 times"],
-      32 => ["Free Buffet", false, "Raped 1,000,000 times"],
-      
-      33 => ["Got Milk?", false, "Produced 1 liter of milk"],
-      34 => ["Udderly delicious", false, "Produced 1,000 liter of milk"],
-      35 => ["The Milky Way", false, "Produced 1,000,000 liter of milk"],
-
-      36 => ["Zidane", false, "Rescue Vivi! She needs help!"],
-      37 => ["For the Horde!", false, "Enslaved by The Horde"],
-      38 => ["The Beast's Pet", false, "Enslaved by an Orc Soldier"],      
-      39 => ["Lost Lost Number", false, "Became Lost Number's mascot"],
-      40 => ["White Knight", false, "Rescue Princess Elisabeth"],
-      41 => ["Green Kids", false, "Enslaved for Orc Breeding"]
-    }
+    @data_achievements = {}
+    load_data
   end
-  
-  def Achievements
-    return @Achievements
+  # Get achievement instance
+  def self.instance
+    return (@@instance ||= self.new)
+  end
+  # Get achievement at index
+  def [](index)
+    return get(index)
+  end
+  # Get achievement at index
+  def get(index)
+    return @data_achievements[index]
+  end
+  # Return number of achievements
+  def size
+    return @data_achievements.length
+  end
+  # Load store achievement data
+  def load_data
+    begin
+      fi = File.open(SAVE_FILE, "rb")
+      completes = Marshal.load(fi)
+    rescue
+      completes = []
+    end
+    ACHIEVEMENTS.each do |index, content|
+      title, passed, description = content
+      completed = completes.include?(index)
+      @data_achievements[index] = Achievement.new(title, description, completed)
+    end
+  end
+  # Save cleared achievements to SAVE_FILE
+  def save
+    cleared_achievements = []
+    @data_achievements.each do |index, achievement|
+      cleared_achievements << index if achievement.completed?
+    end
+    fo = File.open(SAVE_FILE, "wb")
+    begin
+      Marshal.dump(cleared_achievements, fo)
+    ensure
+      fo.close
+    end
+  end
+  # Remove all achievements
+  def clear
+    @data_achievements = nil
+  end
+end
+# An achievement
+class Achievement
+  attr_reader :title
+  attr_reader :description
+  attr_reader :completed
+  def initialize(title, description, completed = false)
+    @title = title
+    @description = description
+    @completed = completed
+  end
+  # Has the achievement been unlocked?
+  def completed?
+    return @completed
+  end
+  # Set the achievement's unlocked state
+  def completed=(value)
+    return @completed = value
   end
 end
 
 class Window_Achievement_Complete < Window_NoModal
-  
   def initialize
     super(70, 150,400, 55)
     @queue = []
+    @achievements = Achievements.instance
   end
-  
   def show_achievement(id)
-    return if !$game_system.Achievements.include?(id)
+    return if @achievements[id] == nil
     @queue.push(id)
   end
-
   def update
     if !@queue.empty?
       if !self.visible
         achiev = @queue.shift
-        show_message("Achievement! [" + $game_system.Achievements[achiev][0] + "] obtained!", 180)
+        show_message("Achievement! [" + @achievements[achiev].title + "] obtained!", 180)
         Sound.play_recovery
       end
     end
     super
   end
-  
 end
 
 class Scene_Map < Scene_Base
@@ -99,12 +161,14 @@ class Scene_Map < Scene_Base
   
   def start
     emt_start
+    @achievements = Achievements.instance
     @achievement_complete_window = Window_Achievement_Complete.new
   end
 
   def complete_achievement(id)
-    return unless !$game_system.Achievements[id][1]
-    $game_system.Achievements[id][1] = true
+    return unless !@achievements[id].completed?
+    @achievements[id].completed = true
+    @achievements.save
     @achievement_complete_window.show_achievement(id)
   end
   
@@ -137,7 +201,7 @@ class Window_Achievements < Window_Selectable
     super(x, y, width, height)
     @column_max = 2
     self.index = 0
-    @data = $game_system.Achievements
+    @data = Achievements.instance
     @item_max = @data.size
     create_contents
     for i in 0...@data.size
@@ -155,15 +219,15 @@ class Window_Achievements < Window_Selectable
     if achievement != nil
       rect.width -= 4
       self.contents.font.color = normal_color
-      self.contents.font.color.alpha = @data[index][1] ? 255 : 128
-      self.contents.draw_text(rect.x + 24, rect.y, 172, WLH, @data[index][0])
+      self.contents.font.color.alpha = @data[index].completed? ? 255 : 128
+      self.contents.draw_text(rect.x + 24, rect.y, 172, WLH, @data[index].title)
     end
   end
   #--------------------------------------------------------------------------
   # * Update Help Text
   #--------------------------------------------------------------------------
   def update_help
-    @help_window.set_text(@data[self.index][2])
+    @help_window.set_text(@data[self.index].description)
   end
 end
 
